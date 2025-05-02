f41403f78a27fa19ad333532a03bcb845fbe59a4
se0yu <pr924133@gmail.com>
2025-05-02 15:31:25 +0900

    1-5: AOP의 이해 1. 대상 클래스, 메서드 변경 2. @After 에서 @Before 로 변경 3. 메서드 실행 전 aop가 동작하므로 메서드명도 수정함

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -17,9 +18,11 @@ import java.time.LocalDateTime;
 public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
-
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
-    public void logAfterChangeUserRole(JoinPoint joinPoint) {
+    // 1. 대상 클래스, 메서드 변경
+    // 2. @After 에서 @Before 로 변경
+    // 3. 메서드 실행 전 aop가 동작하므로 메서드명도 수정함
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
+    public void logBeforeChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
